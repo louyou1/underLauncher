8500a4d0469ec72834d8a5e82393e83a253a9861
lou-you <louyouast@gmail.com>
2024-04-27 11:15:25 +0200

    1.3.1

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = 'a556038e-3dea-443d-82f0-2756f9873258'
 // SEE NOTE ABOVE.
 
 
